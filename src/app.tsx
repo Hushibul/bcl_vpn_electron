a02442525184fac189b4fb942bceca4c8166d2e2
Hushibul <nayeemninz786@gmail.com>
2023-12-26 20:12:23 +0600

    Tailwind added

--- src/app.tsx
+++ src/app.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<h2 className='text-red-600'>Hello from React!</h2>);
